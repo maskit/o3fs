94ee32b3144a651842ffecfb30be39f35ae97c4a
Masakazu Kitajo <m4sk17@gmail.com>
2012-02-28 00:47:13 +0900

    Added some client side implementations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
-YUI().use('node', 'event', function (Y) {
+YUI().use('node', 'event', 'io', 'json', 'node-menunav', 'view', function (Y) {
+	var BrowserAppView;
 	
+	Y.one('#browser-menu').plug(Y.Plugin.NodeMenuNav);
+	BrowserAppView = Y.BrowserAppView = Y.Base.create('browserAppView', Y.View, [], {
+		container: Y.one('#browser-app'),
+		events: {
+			'#browser-menu-storage-mount': {click: 'mount'},
+			'#browser-menu-storage-unmount': {click: 'unmount'},
+			'#browser-menu-storage-list': {click: 'listStorage'},
+		},
+		// --- Application ---
+		mount: function(e) {
+			Y.io('/api/V1/alive', {
+				on: {
+					success: function(tx, res, args) {
+						var data = Y.JSON.parse(res.responseText);
+						console.log(data);
+					},
+					failure: function(tx, res, args) {
+					}
+				}
+			});
+		}
+		
+	});
+	new BrowserAppView();
 });
